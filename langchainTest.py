227e53ef8251f914d08536deb0bf5cb6e0d7c7d6
Samuel Kang <51842483+samkang8293@users.noreply.github.com>
2024-09-06 23:05:27 -0400

    Inst LLM wrapper to interact with OpenAI models

--- langchainTest.py
+++ langchainTest.py
@@ -1,3 +1,18 @@
 from dotenv import load_dotenv, find_dotenv
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+from langchain.chat_models import ChatOpenAI
 
 load_dotenv(find_dotenv())
+
+chat = ChatOpenAI(model_name="gpt-4", temperature=0.3)
+messages = [
+    SystemMessage(content="You are an expert data scientist"),
+    HumanMessage(content="Write a Python script that trains a neural network on simulated data")
+]
+response = chat(messages)
+
+print(response.content, end='\n')
